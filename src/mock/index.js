d870132d695178532d0ce0425470885729e7c74b
ZFu Chen <www.2849007069@outlook.com>
2021-03-03 02:07:31 +0800

    修复vercel编译失败问题

--- src/mock/index.js
+++ src/mock/index.js
@@ -21,13 +21,13 @@ function mockXHR() {
 		return function(options) {
 			let result = null;
 			if (respond instanceof Function) {
-				const { body, type, url } = options;
+				// const { body, type, url } = options;
 				// https://expressjs.com/en/4x/api.html#req
-				result = respond({
-					method: type,
-					body: JSON.parse(body),
-					// query: param2Obj(url)
-				});
+				// result = respond({
+				// 	method: type,
+				// 	body: JSON.parse(body),
+				// 	// query: param2Obj(url)
+				// });
 			} else {
 				result = respond;
 			}
